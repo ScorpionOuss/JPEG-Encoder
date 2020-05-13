ed376111b476f1649e0fab9359ff1d9500aab293
Maxime Ollivier <Maxime.Ollivier2@grenoble-inp.org>
2020-05-13 11:15:24 +0200

    Magnitude correct

--- jpeg/src/magnitude.c
+++ jpeg/src/magnitude.c
@@ -33,7 +33,7 @@ int32_t num_magnitude(int32_t entier, int32_t magnitude)
         return entier;
     }
     else {
-        return puissance(2, magnitude) - abs(entier);
+        return puissance(2, magnitude) - abs(entier) -1;
     }
 }
 
@@ -41,13 +41,13 @@ int32_t num_magnitude(int32_t entier, int32_t magnitude)
 int main(void)
 {
     int32_t a;
-    a = calc_magnitude(-10);
-    printf("%i, %i\n", -10, a);
-    printf("%i \n", num_magnitude(-10, a));
+    a = calc_magnitude(-1);
+    printf("%i, %i\n", -1, a);
+    printf("%i \n", num_magnitude(-1, a));
 
-    a = calc_magnitude(77);
-    printf("%i, %i \n", 77, a);
-    printf("%i \n", num_magnitude(77, a));
+    a = calc_magnitude(-8);
+    printf("%i, %i \n", -8, a);
+    printf("%i \n", num_magnitude(-8, a));
 
  return EXIT_SUCCESS;
 }
