e79434f6773e01da743edce7ff9917bc89c1d7bc
Xavier Bonetti <bonettix@pcserveur.ensimag.fr>
2020-05-20 23:26:09 +0200

    Inchallah

--- jpeg/src/creation_jpeg.c
+++ jpeg/src/creation_jpeg.c
@@ -29,19 +29,19 @@ int32_t ***Y_Cb_Cr(int32_t*** data, int32_t longueur, int32_t largeur)
 
   // On définit trois pointeurs pour les calculs, un sur chaque composante RGB
   
-  int32_t R;
-  int32_t G;
-  int32_t B;
+  uint8_t R;
+  uint8_t G;
+  uint8_t B;
 
   for (size_t lon = 0; lon < longueur; lon++) {
     for (size_t lar = 0; lar < largeur; lar++) {
         R = data[lon][lar][0];
         G = data[lon][lar][1];
         B = data[lon][lar][2];
-
-        data_nouv[lon][lar][0] = (int) 0.299 * (float) R + 0.587 * (float) G + 0.114 * (float) B;
-        data_nouv[lon][lar][1] = (int) -0.1687 * (float) R - 0.3313 * (float) G + 0.5 * (float) B + 128;
-        data_nouv[lon][lar][2] = (int) 0.5 * (float) R - 0.4187* (float) G - 0.0813 * (float) B + 128;
+        data_nouv[lon][lar][0] = (int) round(0.299 * (float) R + 0.587 * (float) G + 0.114 * (float) B);
+        data_nouv[lon][lar][1] = (int) round(-0.1687 * (float) R - 0.3313 * (float) G + 0.5 * (float) B + 128);
+        data_nouv[lon][lar][2] = (int) round(0.5 * (float) R - 0.4187* (float) G - 0.0813 * (float) B + 128);
+        printf("R %d G %d B %d deviennent", R, G, B);
 
     }
   }
