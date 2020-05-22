db0d0f2e720b07eeb0a8963bf27c73678a59b403
Xavier Bonetti <bonettix@pcserveur.ensimag.fr>
2020-05-22 12:48:57 +0200

    0

--- jpeg/src/creation_jpeg.c
+++ jpeg/src/creation_jpeg.c
@@ -17,6 +17,94 @@ struct Entete {
     int32_t range_couleur;
 };
 
+
+int32_t ***bonne_taille_couleur_ssechantillonnage(int32_t ***data, int32_t* entete, int8_t echantillonage)
+//Transforme l'image de sorte que ses dimensions soient un multiple de 8
+{
+    int8_t na = 8;
+    int8_t no = 8;
+    
+    // On met les bons parametres de dimensions des MCU, 8 ou 16
+    
+    if (echantillonage == 1){
+        na = 16;
+    }
+    else if (echantillonage == 2){
+        na = 16;
+        no = 16;
+    }
+
+    // On calcule les bonnes dimensions
+    int bonne_abscisse = *entete;
+    if (*entete % na != 0)
+    {
+    bonne_abscisse += na - *entete % na;
+    }
+    
+    int bonne_ordonnee = *(entete+1);
+    if (*(entete+1) % no != 0)
+    {
+    bonne_ordonnee += (no - *(entete+1) % no);
+    }
+        
+    // On créé un nouveau tableau data aux bonnes dimensions
+    int32_t*** data_new = malloc(bonne_ordonnee*sizeof(int32_t*));
+    for (size_t i = 0; i < bonne_ordonnee; i++) {
+      
+      data_new[i] = malloc(bonne_abscisse*sizeof(int32_t*));
+      for (size_t j = 0; j < bonne_abscisse; j++)
+      {
+        data_new[i][j] = malloc(3*sizeof(int32_t));
+      }
+    }
+    
+    // On le remplit avec les anciennes valeurs
+    for (size_t y = 0; y < *(entete+1); y++) {
+      for (size_t x = 0; x < *(entete); x++) {
+          for (size_t z = 0; z<3; z++)
+          {
+          data_new[y][x][z] = data[y][x][z];
+          }
+      }
+    }
+        
+    // On met les nouvelles valeurs
+    // En bas d'abord
+     for (size_t y = *(entete+1); y < bonne_ordonnee; y++) {
+        for (size_t x = 0; x < *entete; x++) {
+          for (size_t z = 0; z<3; z++)
+          {
+            data_new[y][x][z] = data[*(entete+1)-1][x][z];
+          }
+        }
+    }
+    // Puis à droite
+     for (size_t y = 0; y < bonne_ordonnee; y++) {
+        for (size_t x = *entete; x < bonne_abscisse; x++) {
+          for (size_t z = 0; z<3; z++)
+          {
+            data_new[y][x][z] = data[*(entete+1)-1][*entete -1][z];
+          }
+        }
+    }
+
+    
+    return data_new;
+
+}
+
+
+int max(int t[2])
+{
+    if (t[0]>t[1])
+    {
+    return t[0];
+    }
+    else
+    {
+    return t[1];
+    }
+}
 int32_t ***Y_Cb_Cr(int32_t*** data, int32_t longueur, int32_t largeur)
 {
   int32_t*** data_nouv = malloc(longueur*sizeof(int32_t**)); 
@@ -367,14 +455,17 @@ struct jpeg *creation_jpeg_couleur(int32_t *parametres)
     jpeg_set_huffman_table(image, acdc, cc, htable);
     }
     }
-    for (int cc=0; cc<3; cc++)
-    {
+    int cc = 0;
     for (int i=0; i<2; i++)
     {
     jpeg_set_sampling_factor(image,cc, i, 1);
     }
-    }
-        
+    cc=1;
+    jpeg_set_sampling_factor(image,cc, 1, 2);
+    jpeg_set_sampling_factor(image,cc, 2, 1);
+    cc=2;
+    jpeg_set_sampling_factor(image,cc, 1, 2);
+    jpeg_set_sampling_factor(image,cc, 2, 1);
     uint8_t *qtables;
     qtables = &quantification_table_Y;
     jpeg_set_quantization_table(image, 0, qtables);
@@ -470,8 +561,10 @@ int32_t ***zigzag_bloc(int32_t** ptr_sur_tab, int* entete)
 
     return ptr_sur_tab_retour_ordonnee;
 }
-int32_t ***zigzag_bloc_couleur(int32_t*** ptr_sur_tab, int* entete)
+int32_t ***zigzag_bloc_couleur(int32_t*** ptr_sur_tab, int* entete, int echantillonage)
 {
+    if (echantillonage == 0)
+    {
     // On calcule les bonnes dimensions
     int largeur = *entete;
     if (*entete % 8 != 0)
@@ -513,21 +606,106 @@ int32_t ***zigzag_bloc_couleur(int32_t*** ptr_sur_tab, int* entete)
     }
     }
     }
-
-    int32_t ***ptr_sur_tab_retour_ordonnee = malloc(((int) largeur/8 )*((int) hauteur / 8)*3 *sizeof(int32_t**));
-    for (int i=0; i<(3*(int) largeur/8 )*((int) hauteur / 8);i++)
+    }
+    else
     {
-        ptr_sur_tab_retour_ordonnee[i] = ptr_sur_tab_retour[i/(3*largeur/8)][(i)%(3*largeur/8)];
+    // On calcule les bonnes dimensions avec un echantillonage
+ 
+    int largeur = *entete;
+    if (*entete % 16 != 0)
+    {
+    largeur += 16 - *entete % 16;
+    }
+    
+    int hauteur = *(entete+1);
+    if (*(entete+1) % 8 != 0)
+    {
+    hauteur += (8 - *(entete+1) % 8);
+    }
+    int32_t ****ptr_sur_tab_retour = malloc(((int) hauteur / 8)* sizeof(int32_t***));
+    for (size_t i3 = 0; i3 < (int) hauteur/8; i3++) {
+      ptr_sur_tab_retour[i3] = malloc(((int) largeur*3/8)*sizeof(int32_t**));
+    } 
+    for (int i = 0; i<hauteur;i=i+8)
+    {
+    for (int j=0; j<largeur; j=j+8)
+    {
+    
+
+    
+    for (int i2= 0; i2<3; i2++)
+    {
+    int32_t** data = malloc(8*sizeof(int32_t*));
+    for (size_t i3 = 0; i3 < 8; i3++) {
+      data[i3] = malloc(8*sizeof(int32_t));
+      } 
+    for (int i1 = 0; i1<8; i1++)
+    {
+    for (int j1 = 0; j1<8; j1++)
+    {
+        //printf("pour i: %d et j: %d  avec hauteur %d et largeur %d; \n",i, j, hauteur, largeur);
+        if ((j+j1+8)<largeur)
+        {
+        if (i%3==0)
+        {
+
+        data[i1][j1] = ptr_sur_tab[i+i1][j+j1][i2];
+        }
+        else
+        {
+        data[i1][j1] = (ptr_sur_tab[i+i1][j+j1][i2]+ptr_sur_tab[i+i1][j+j1+8][i2])/2;//*(*(ptr_sur_tab+i+i1)+j+j1);
+        }
+        }
+        else
+        {
+        data[i1][j1] = ptr_sur_tab[i+i1][j+j1][i2];//*(*(ptr_sur_tab+i+i1)+j+j1);
+        }
+    }
+    }
+    ptr_sur_tab_retour[i/8][3*j/8+i2] = data;
+    }
     }
 
+    }
+    printf("suite ZigZag \n");    
+    int j = 0;
+    int32_t ***ptr_sur_tab_retour_ordonnee = malloc(((int) largeur/16 )*((int) hauteur / 8)*4 *sizeof(int32_t**));
+    for (int i=0; i<4*((int) (largeur/16) )*((int) hauteur / 8);i++)
+    {
+        if (i%4==0)
+        {
+        ptr_sur_tab_retour_ordonnee[i] = ptr_sur_tab_retour[j/(3*largeur/8)][(j)%(3*largeur/8)];
+        j=j+3;
+
+        }
+        if (i%4==1)
+        {
+        ptr_sur_tab_retour_ordonnee[i] = ptr_sur_tab_retour[j/(3*largeur/8)][(j)%(3*largeur/8)];
+        j=j-2;
+        }
+        if (i%4==2)
+        {
+        ptr_sur_tab_retour_ordonnee[i] = ptr_sur_tab_retour[j/(3*largeur/8)][(j)%(3*largeur/8)];
+        j++;
+        }
+        if (i%4==3)
+        {
+        ptr_sur_tab_retour_ordonnee[i] = ptr_sur_tab_retour[j/(3*largeur/8)][(j)%(3*largeur/8)];
+        j=j+4;
+        }
+    }
+
+    printf(" on fini avec j=%d et %d \n", j, (largeur/16)*(hauteur/8)*4);
 
     return ptr_sur_tab_retour_ordonnee;
-}
+    }
+    }
 
 
 int main(int argc, char const *argv[])
 {
     int32_t* entete = NULL; // entête du fichier pgm
+    int echantillonage = 1;
     int32_t*** ptr_sur_tab_MCU;
     int nbr_MCU;
     struct Entete *entete1;  // entête du fichier pgm
@@ -593,9 +771,11 @@ int main(int argc, char const *argv[])
     printf("hello1\n");
     data_Y = Y_Cb_Cr(data, entete1-> longueur, entete1->largeur);
     printf("hello1\n");
+    if (echantillonage == 0)
+    {
     data_new = bonne_taille_couleur(data_Y, entete);
     printf("hello 3 \n");
-    ptr_sur_tab_MCU = zigzag_bloc_couleur(data_new, entete); 
+    ptr_sur_tab_MCU = zigzag_bloc_couleur(data_new, entete, 0); 
     printf("hello 3 \n");
     int largeur = *entete;
     if (*entete % 8 != 0)
@@ -628,7 +808,49 @@ int main(int argc, char const *argv[])
     jpeg_write_footer(image);
     jpeg_destroy(image);
     /* fin*/
+    }
+    else
+    {
+    data_new = bonne_taille_couleur_ssechantillonnage(data_Y, entete, 1);
+    ptr_sur_tab_MCU = zigzag_bloc_couleur(data_new, entete,1); 
+    printf("hello 3 \n");
+    int largeur = *entete;
+    if (*entete % 16 != 0)
+    {
+    largeur += 16 - *entete % 16;
+    }
+    
+    int hauteur = *(entete+1);
+    if (*(entete+1) % 8 != 0)
+    {
+    hauteur += (8 - *(entete+1) % 8);
+    }
 
+    
+    /*DCT + ZigZagi*/
+    nbr_MCU = (largeur/16)*(hauteur/8)*4;
+    int32_t* ptr_tab_data; 
+    int8_t taille = 64;
+    printf("hello 3 \n");
+    jpeg_write_header(image);
+    printf("hello 3 \n");
+    
+    for (int i=0; i<nbr_MCU;i++)
+    {
+    int tab[2] ={i%4-1, 0} ;
+    ptr_tab_data = operations_dct_quantification_puis_zig_zag(*(ptr_sur_tab_MCU+i), max(tab)); 
+    /* Huffman  */
+    gestion_compression(image, ptr_tab_data, taille, exDC[max(tab)],max(tab));
+
+    exDC[max(tab)] = *ptr_tab_data;
+    printf("%d %d %d %d \n", exDC[0], exDC[1], exDC[2], max(tab));
+    }
+    jpeg_write_footer(image);
+    jpeg_destroy(image);
+    /* fin*/
+
+
+    }
     };
 
     return 0;
